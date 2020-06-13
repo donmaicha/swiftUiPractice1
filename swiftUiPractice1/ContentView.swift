460d6631d2d54de2980f544cd3ebc1149c67fc97
donmaicha <genmaicha77783@gmail.com>
2020-06-13 19:53:57 +0900

    swiftUIでテキスト部品を使って遊んでみた

--- swiftUiPractice1/ContentView.swift
+++ swiftUiPractice1/ContentView.swift
@@ -10,7 +10,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Bicycle for the Mind")
+                .font(.title)
+                .fontWeight(.thin)
+            
+            Text("知性の自転車")
+                .foregroundColor(Color.red)
+            
+            Text("春はあけぼの。やうやう白くなりゆく、山ぎは少しあかりて、紫だちたる雲の細くたなびきたる。")
+                .lineLimit(2) // 自動改行のlimit。超える場合は「...」と表示される
+                .frame(width: 200.0) // 横幅いっぱいで自動開業するが、幅を指定することで、指定幅で自動開業するようになる
+            
+            Text("The quick brown fox jumps over the lazy dog.")
+                .font(.largeTitle)
+                .multilineTextAlignment(.trailing) // 複数行表示で右寄せ
+                .frame(width: 200.0)
+                .border(Color.green, width: 5)
+            
+            Text("Hello World")
+                .font(.system(size:80))
+            
+            VStack {
+                Text("春はあけぼの")
+                Text("夏は夜")
+                    .foregroundColor(.red)
+                Text("秋は夕暮れ")
+                Text("冬はつとめて")
+            }
+            .font(.title)
+            .foregroundColor(.blue)
+        }
     }
 }
 
